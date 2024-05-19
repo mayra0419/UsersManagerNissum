dc6e17606337e8e94080e7137cc6321b3b6cb0e9
mayra0419 <mayra9919@gmail.com>
2024-05-19 18:52:43 -0500

    Adding swagger generation

--- src/main/java/com/mayra0419/usersmanager/controller/UserController.java
+++ src/main/java/com/mayra0419/usersmanager/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/user")
 public class UserController implements UserApi {
 
     private final UserService userService;
